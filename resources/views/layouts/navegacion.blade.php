a0f4b40fde0dad2115188db0889cb8c2d3184dce
RaquelRuiz4 <raquelruiz121295@gmail.com>
2023-06-11 18:26:26 +0200

    estilo login & muestra nombre de usuario en perfil

--- resources/views/layouts/navegacion.blade.php
+++ resources/views/layouts/navegacion.blade.php
@@ -49,9 +49,6 @@
                     </x-nav-link>
                     }
                 @endif
-                    <!-- <x-nav-link :href="route('veterinarios')" :active="request()->routeIs('veterinarios')">
-                        {{ __('Veterinarios') }}
-                    </x-nav-link> -->
                 </div>
             </div>
 
@@ -63,8 +60,7 @@
                         <x-slot name="trigger">
                             <button
                                 class="inline-flex items-center px-3.5 py-2.5 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{ Auth::check() && Auth::user()->name }}</div>
-
+                                <div>{{ Auth::user()->nombre }}</div>
                                 <div>
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20">
