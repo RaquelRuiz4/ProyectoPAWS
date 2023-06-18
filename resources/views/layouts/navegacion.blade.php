244bd7008fc94315d962b91dc48311a2471b5213
RaquelRuiz4 <raquelruiz121295@gmail.com>
2023-06-18 22:39:46 +0200

    logo

--- resources/views/layouts/navegacion.blade.php
+++ resources/views/layouts/navegacion.blade.php
@@ -9,7 +9,7 @@
                     <a href="{{ route('inicio') }}">
                         <!-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> -->
                         <img class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
-                            src={{ asset('../resources/images/logo1.png') }} />
+                            src={{ asset('images/imagen/logo1.png') }} />
                     </a>
                 </div>
 
